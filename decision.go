d4860756cb38c645f0c3895f652ac03dca244bcf
Hou Junjie <houjunjie@supremind.com>
2020-09-10 17:53:34 +0800

    decision makers

--- decision.go
+++ decision.go
@@ -1,421 +1,104 @@
 package rbac
 
-import (
-	"sync"
-)
-
-type DecisionMaker struct {
-	sg Grouping // optional, subject grouping
-	og Grouping // optional, object grouping
-	p  Permission
-
-	sync.RWMutex
-	uap map[User]map[Article]Action // cache user -> article -> action permissions
+type DecisionMaker interface {
+	Subjector
+	Objector
+	Permission
 }
 
-// SubjectJoin joins a user or a sub role to a role
-func (dm *DecisionMaker) SubjectJoin(sub Subject, role Role) error {
-	if e := dm.sg.Join(sub, role); e != nil {
-		return e
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	switch sub.(type) {
-	case User:
-		return dm.completeUserByRole(sub.(User), role)
-
-	case Role:
-		users, e := dm.sg.IndividualsIn(sub.(Role))
-		if e != nil {
-			return e
-		}
-		for ind := range users {
-			if e := dm.completeUserByRole(ind.(User), role); e != nil {
-				return e
-			}
-		}
-
-	default:
-		return ErrInvlaidSubject
-	}
-
-	return nil
+// Subjector manages subjects and groupings of subjects
+type Subjector interface {
+	// SubjectJoin joins a user or a sub role to a role
+	SubjectJoin(sub Subject, role Role) error
+	// SubjectLeave removes a user or a sub role from a role
+	SubjectLeave(sub Subject, role Role) error
+	// RemoveUser removes a user and all policies about it
+	RemoveUser(user User) error
+	// RemoveRole removes a role and all policies about it
+	RemoveRole(role Role) error
+	// Subjects returns the GroupingReader interface for subjects
+	Subjects() GroupingReader
 }
 
-// SubjectLeave removes a user or a sub role from a role
-func (dm *DecisionMaker) SubjectLeave(sub Subject, role Role) error {
-	if e := dm.sg.Leave(sub, role); e != nil {
-		return e
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	switch sub.(type) {
-	case User:
-		return dm.rebuildUser(sub.(User))
-
-	case Role:
-		users, e := dm.sg.IndividualsIn(sub.(Role))
-		if e != nil {
-			return e
-		}
-		for user := range users {
-			if e := dm.rebuildUser(user.(User)); e != nil {
-				return e
-			}
-		}
-
-	default:
-		return ErrInvlaidSubject
-	}
-
-	return nil
+// Objector manages objects and groupings of objects
+type Objector interface {
+	// ObjectJoin joins an article or a sub category to a category
+	ObjectJoin(obj Object, cat Category) error
+	// ObjectLeave removes an article or a sub category from a category
+	ObjectLeave(obj Object, cat Category) error
+	// RemoveArticle removes an article and all polices about it
+	RemoveArticle(art Article) error
+	// RemoveCategory removes a category and all polices about it
+	RemoveCategory(cat Category) error
+	// Objects returns the GroupingReader interface for objects
+	Objects() GroupingReader
 }
 
-// RemoveUser removes a user and all policies about it
-func (dm *DecisionMaker) RemoveUser(user User) error {
-	dm.Lock()
-	defer dm.Unlock()
+func NewDecisionMaker(opts ...DecisionMakerOption) DecisionMaker {
+	dm := &decisionMaker{
+		uap: make(map[User]map[Article]Action),
+	}
 
-	delete(dm.uap, user)
-	return dm.sg.RemoveIndividual(user)
+	cfg := &decisionMakerConfig{}
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	if cfg.subjectsGrouped {
+		dm.sg = newFatGrouping()
+		if cfg.synced {
+			dm.sg = newSyncedGrouping(dm.sg)
+		}
+	}
+	if cfg.objectsGrouped {
+		dm.og = newFatGrouping()
+		if cfg.synced {
+			dm.og = newSyncedGrouping(dm.og)
+		}
+	}
+
+	dm.p = newThinPermission()
+	if dm.sg != nil {
+		if dm.og != nil {
+			dm.p = newBothGroupedPermission(dm.sg, dm.og, dm.p)
+		} else {
+			dm.p = newSubjectGroupedPermission(dm.sg, dm.p)
+		}
+	} else {
+		if dm.og != nil {
+			dm.p = newObjectGroupedPermission(dm.og, dm.p)
+		}
+	}
+	if cfg.synced {
+		dm.p = newSyncedPermission(dm.p)
+		return newSyncedDecisionMaker(dm)
+	}
+
+	return dm
 }
 
-// RemoveRole removes a role and all policies about it
-func (dm *DecisionMaker) RemoveRole(role Role) error {
-	users, e := dm.sg.IndividualsIn(role)
-	if e != nil {
-		return e
-	}
-
-	if e := dm.sg.RemoveGroup(role); e != nil {
-		return e
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	for user := range users {
-		if e := dm.rebuildUser(user.(User)); e != nil {
-			return e
-		}
-	}
-
-	return nil
+type decisionMakerConfig struct {
+	synced          bool
+	subjectsGrouped bool
+	objectsGrouped  bool
 }
 
-// ObjectJoin joins an article or a sub category to a category
-func (dm *DecisionMaker) ObjectJoin(obj Object, cat Category) error {
-	if e := dm.og.Join(obj, cat); e != nil {
-		return e
+type DecisionMakerOption func(*decisionMakerConfig)
+
+func WithSubjectGrouping() DecisionMakerOption {
+	return func(c *decisionMakerConfig) {
+		c.subjectsGrouped = true
 	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	switch obj.(type) {
-	case Article:
-		return dm.completeArticleByCategory(obj.(Article), cat)
-
-	case Category:
-		arts, e := dm.og.IndividualsIn(obj.(Category))
-		if e != nil {
-			for art := range arts {
-				if e := dm.completeArticleByCategory(art.(Article), cat); e != nil {
-					return e
-				}
-			}
-		}
-
-	default:
-		return ErrInvlaidObject
-	}
-
-	return nil
 }
 
-// ObjectLeave removes an article or a sub category from a category
-func (dm *DecisionMaker) ObjectLeave(obj Object, cat Category) error {
-	if e := dm.og.Leave(obj, cat); e != nil {
-		return e
+func WithObjectGrouping() DecisionMakerOption {
+	return func(c *decisionMakerConfig) {
+		c.objectsGrouped = true
 	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	switch obj.(type) {
-	case Article:
-		return dm.rebuildArticle(obj.(Article))
-
-	case Category:
-		arts, e := dm.og.IndividualsIn(obj.(Category))
-		if e != nil {
-			return e
-		}
-		for art := range arts {
-			if e := dm.rebuildArticle(art.(Article)); e != nil {
-				return e
-			}
-		}
-
-	default:
-		return ErrInvlaidObject
-	}
-
-	return nil
 }
 
-// RemoveArticle removes an article and all polices about it
-func (dm *DecisionMaker) RemoveArticle(art Article) error {
-	if e := dm.og.RemoveIndividual(art); e != nil {
-		return e
+func WithSynced() DecisionMakerOption {
+	return func(c *decisionMakerConfig) {
+		c.synced = true
 	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	for _, perms := range dm.uap {
-		delete(perms, art)
-	}
-
-	return nil
-}
-
-// RemoveCategory removes a category and all polices about it
-func (dm *DecisionMaker) RemoveCategory(cat Category) error {
-	arts, e := dm.og.IndividualsIn(cat)
-	if e != nil {
-		return e
-	}
-
-	if e := dm.og.RemoveGroup(cat); e != nil {
-		return e
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	for art := range arts {
-		if e := dm.rebuildArticle(art.(Article)); e != nil {
-			return e
-		}
-	}
-
-	return nil
-}
-
-// Permit subject to perform action on object
-func (dm *DecisionMaker) Permit(sub Subject, obj Object, act Action) error {
-	if e := dm.p.Permit(sub, obj, act); e != nil {
-		return e
-	}
-
-	users := make(map[Individual]struct{}, 1)
-	arts := make(map[Individual]struct{})
-
-	switch sub.(type) {
-	case User:
-		users[sub.(User)] = struct{}{}
-
-	case Role:
-		us, e := dm.sg.IndividualsIn(sub.(Role))
-		if e != nil {
-			return e
-		}
-		users = us
-
-	default:
-		return ErrInvlaidSubject
-	}
-
-	switch obj.(type) {
-	case Article:
-		arts[obj.(Article)] = struct{}{}
-
-	case Category:
-		as, e := dm.og.IndividualsIn(obj.(Category))
-		if e != nil {
-			return e
-		}
-		arts = as
-
-	default:
-		return ErrInvlaidObject
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	for u := range users {
-		user := u.(User)
-		if dm.uap[user] == nil {
-			dm.uap[user] = make(map[Article]Action)
-		}
-
-		for a := range arts {
-			art := a.(Article)
-			dm.uap[user][art] |= act
-		}
-	}
-
-	return nil
-}
-
-// Revoke permission for subject to perform action on object
-func (dm *DecisionMaker) Revoke(sub Subject, obj Object, act Action) error {
-	if e := dm.p.Revoke(sub, obj, act); e != nil {
-		return e
-	}
-
-	removeByUser := func(user User) error {
-		if dm.uap[user] == nil {
-			return nil
-		}
-
-		switch obj.(type) {
-		case Article:
-			delete(dm.uap[user], obj.(Article))
-
-		case Category:
-			arts, e := dm.og.IndividualsIn(obj.(Category))
-			if e != nil {
-				return e
-			}
-			for art := range arts {
-				delete(dm.uap[user], art.(Article))
-			}
-
-		default:
-			return ErrInvlaidObject
-		}
-
-		return nil
-	}
-
-	dm.Lock()
-	defer dm.Unlock()
-
-	switch sub.(type) {
-	case User:
-		return removeByUser(sub.(User))
-
-	case Role:
-		users, e := dm.sg.IndividualsIn(sub.(Role))
-		if e != nil {
-			return e
-		}
-
-		for user := range users {
-			if e := removeByUser(user.(User)); e != nil {
-				return e
-			}
-		}
-
-	default:
-		return ErrInvlaidObject
-	}
-
-	return nil
-}
-
-// Shall subject to perform action on object
-func (dm *DecisionMaker) Shall(sub Subject, obj Object, act Action) (bool, error) {
-	dm.RLock()
-	defer dm.RUnlock()
-
-	if user, ok := sub.(User); ok {
-		if art, ok := obj.(Article); ok {
-			if dm.uap[user] != nil {
-				return dm.uap[user][art].Includes(act), nil
-			}
-			return false, nil
-		}
-	}
-
-	return dm.p.Shall(sub, obj, act)
-}
-
-// PermissionsOn object for all subjects
-func (dm *DecisionMaker) PermissionsOn(obj Object) (map[Subject]Action, error) {
-	return dm.p.PermissionsOn(obj)
-}
-
-// PermissionsFor subject on all objects
-func (dm *DecisionMaker) PermissionsFor(sub Subject) (map[Object]Action, error) {
-	return dm.p.PermissionsFor(sub)
-}
-
-// PermittedActions for subject on object
-func (dm *DecisionMaker) PermittedActions(sub Subject, obj Object) (Action, error) {
-	return dm.p.PermittedActions(sub, obj)
-}
-
-func (dm *DecisionMaker) rebuildUser(user User) error {
-	roles, e := dm.sg.GroupsOf(user)
-	if e != nil {
-		return e
-	}
-
-	for role := range roles {
-		if e := dm.completeUserByRole(user, role.(Role)); e != nil {
-			return e
-		}
-	}
-
-	return nil
-}
-
-func (dm *DecisionMaker) completeUserByRole(user User, role Role) error {
-	perms, e := dm.p.PermissionsFor(role)
-	if e != nil {
-		return e
-	}
-
-	if dm.uap[user] == nil {
-		dm.uap[user] = make(map[Article]Action, len(perms))
-	}
-
-	for obj, act := range perms {
-		if art, ok := obj.(Article); ok {
-			dm.uap[user][art] |= act
-		}
-	}
-
-	return nil
-}
-
-func (dm *DecisionMaker) rebuildArticle(art Article) error {
-	cats, e := dm.og.GroupsOf(art)
-	if e != nil {
-		return e
-	}
-
-	for cat := range cats {
-		if e := dm.completeArticleByCategory(art, cat.(Category)); e != nil {
-			return e
-		}
-	}
-
-	return nil
-}
-
-func (dm *DecisionMaker) completeArticleByCategory(art Article, cat Category) error {
-	perms, e := dm.p.PermissionsOn(cat)
-	if e != nil {
-		return e
-	}
-
-	for sub, act := range perms {
-		if user, ok := sub.(User); ok {
-			if dm.uap[user] == nil {
-				dm.uap = make(map[User]map[Article]Action, 1)
-			}
-			dm.uap[user][art] |= act
-		}
-	}
-
-	return nil
 }
