49e578c05eb828454b2ae412ac73ff3c17574a41
Hou Junjie <houjunjie@supremind.com>
2020-09-28 20:43:16 +0800

    simpler constructor

--- rbac.go
+++ rbac.go
@@ -2,8 +2,13 @@ package rbac
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/go-logr/logr"
+	"github.com/go-logr/stdr"
 	"github.com/houz42/rbac/internal/authorizer"
 	"github.com/houz42/rbac/internal/grouping"
 	"github.com/houz42/rbac/internal/permission"
@@ -17,33 +22,39 @@ func New(ctx context.Context, opts ...AuthorizerOption) (types.Authorizer, error
 		opt(cfg)
 	}
 
+	if cfg.log == nil {
+		cfg.log = stdr.New(log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile))
+	}
+
 	var sg, og types.Grouping
 	if cfg.sp != nil {
 		var e error
-		sg, e = grouping.NewPersistedGrouping(ctx, nil, cfg.sp)
+		sg, e = grouping.New(ctx, cfg.sp, cfg.log.WithName("subject"))
 		if e != nil {
 			return nil, fmt.Errorf("init subject grouping failed: %w", e)
 		}
 	}
 	if cfg.op != nil {
 		var e error
-		og, e = grouping.NewPersistedGrouping(ctx, nil, cfg.op)
+		og, e = grouping.New(ctx, cfg.op, cfg.log.WithName("object"))
 		if e != nil {
 			return nil, fmt.Errorf("init object grouping failed: %w", e)
 		}
 	}
 
-	var p types.Permission = permission.NewSyncedPermission(nil)
+	var p types.Permission
 	if cfg.pp != nil {
 		var e error
-		p, e = permission.NewPersistedPermission(ctx, cfg.pp)
+		p, e = permission.New(ctx, cfg.pp, cfg.log.WithName("permission"))
 		if e != nil {
 			return nil, fmt.Errorf("init permission failed: %w", e)
 		}
+	} else {
+		return nil, errors.New("empty permission persister")
 	}
 
 	var authz types.Authorizer
-	authz = authorizer.NewSyncedAuthorizer(authorizer.NewAuthorizer(sg, og, p))
+	authz = authorizer.New(sg, og, p)
 	if len(cfg.presets) > 0 {
 		authz = authorizer.NewWithPresetPolices(authz, cfg.presets...)
 	}
@@ -82,12 +93,20 @@ func WithPresetPolices(presets ...types.PresetPolicy) AuthorizerOption {
 	}
 }
 
+// WithLogger sets logger for rbac components
+func WithLogger(l logr.Logger) AuthorizerOption {
+	return func(cfg *AuthorizerConfig) {
+		cfg.log = l
+	}
+}
+
 // AuthorizerConfig works together with AuthorizerOption to control the initialization of authorizer
 type AuthorizerConfig struct {
 	sp      types.GroupingPersister
 	op      types.GroupingPersister
 	pp      types.PermissionPersister
 	presets []types.PresetPolicy
+	log     logr.Logger
 }
 
 // AuthorizerOption controls how to init an authorizer
